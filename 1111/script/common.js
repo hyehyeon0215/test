d9553e83db0476178c73453f010655dd0788f1eb
hyehyeon0215 <qwert0880@naver.com>
2023-11-11 23:11:27 +0900

    load 완료

---
+++ 1111/script/common.js
@@ -0,0 +1,9 @@
+
+$(function() {
+    if ($("header").length > 0) {
+        $("header").load("../include/header.html")
+    }
+    if ($("footer").length > 0) {
+        $("footer").load("../include/footer.html")
+    }
+})
